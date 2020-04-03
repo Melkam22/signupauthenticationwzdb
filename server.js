4e330d331f7153d3734cc25b9729f340cf301a8f
Ashu <ashenshi@gmail.com>
2020-04-03 12:50:34 +0200

    views folder & 5 ejs files added, fontawsome & bootstrap linked

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+
+//require expressLayouts (EJS)
+const expressLayouts = require('express-ejs-layouts');
+
 const app = express();
+
+//EJS, from expressLayouts documentation
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 //importing & requiring my index.js bien venu route
 app.use('/', require('./routes/index'));
