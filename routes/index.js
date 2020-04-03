ee369c50baeaf28dba2dd30091ec9cb76977d6b9
Ashu <ashenshi@gmail.com>
2020-04-03 17:36:06 +0200

    bienvenu.ejs form and route on index.js done

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res)=> res.send('Bienvenu!'))
+router.get('/', (req, res)=> res.render('bienvenu'))
 
 module.exports = router;
