8e0ba634446f721eb275adc6d5457b32c6a8523f
Ashu <ashenshi@gmail.com>
2020-04-09 12:13:50 +0200

    auth.js & ensuredAuthenticated added to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+//importing auth.js to prevent unauthorized logins
+const {ensureAuthenticated} = require('../config/auth');
 
 router.get('/', (req, res)=> res.render('bienvenu'));
 
-//route to dashboard
-router.get('/dashboard', (req, res)=>res.render('dashboard'));
+//route to dashboard @ the end we ll add ensureAuthenticated 
+router.get('/dashboard', ensureAuthenticated, (req, res)=>res.render('dashboard'));
 
 module.exports = router;
