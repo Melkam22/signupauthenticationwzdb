90b363854c4b99fab91a0463b9a900c932a7db8b
Ashu <ashenshi@gmail.com>
2020-04-09 12:44:44 +0200

    login welcome msg added

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,13 @@ const {ensureAuthenticated} = require('../config/auth');
 router.get('/', (req, res)=> res.render('bienvenu'));
 
 //route to dashboard @ the end we ll add ensureAuthenticated 
-router.get('/dashboard', ensureAuthenticated, (req, res)=>res.render('dashboard'));
+/* router.get('/dashboard', ensureAuthenticated, (req, res)=>
+res.render('dashboard')); to add client's name do the below*/
+
+router.get('/dashboard', ensureAuthenticated, (req, res)=>
+res.render('dashboard',{
+    name: req.user.name
+}));
+
 
 module.exports = router;
