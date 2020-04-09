ea546e6abfa44fd4b97f7b2ba2956d973450b0bf
Ashu <ashenshi@gmail.com>
2020-04-09 11:29:21 +0200

    dashboard router added to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res)=> res.render('bienvenu'))
+router.get('/', (req, res)=> res.render('bienvenu'));
+
+//route to dashboard
+router.get('/dashboard', (req, res)=>res.render('dashboard'));
 
 module.exports = router;
